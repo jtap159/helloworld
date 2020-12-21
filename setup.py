25d9c34d043f2f537f0ddaaa2ef95e5502f6d1b5
Jeremy <engjat94@gmail.com>
2020-12-21 05:41:48 -0600

    updated the helloworld package __init__.py, the setup.py with notes, and tested

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jtap159/helloworld",
-    packages=find_packages(),
+    packages=find_packages(),  # needs a folder with __init__()
+    # py_modules=["helloworld"], # use if you only have modules
+    # package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
